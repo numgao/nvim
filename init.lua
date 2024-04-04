35fc7d0dd484c4fa425d7a7889e14ec359771758
gao <paul.gao@gmail.com>
2024-04-04 04:49:18 +0000

    This is commit for Nvim config

---
+++ init.lua
@@ -0,0 +1,59 @@
+-- 配置文件入口,主要负责加载其他配置文件
+require("basic")
+require("keybindings")
+require("autocmd")
+require("plugins")
+
+--插件设置
+require("plugin-config/tokyonight")
+-- require("plugin-config/vscode")
+require("plugin-config/buffer")
+require("plugin-config/nvimtree")
+require("plugin-config/lualine")
+--=require("plugin-config/dashboard")
+require("plugin-config/whichkey")
+--require("plugin-config/toggleterm")
+require("plugin-config/telescope")
+--require("plugin-config/auto_session")
+require("plugin-config/treesitter")
+require("plugin-config/cmp")
+--require("plugin-config/lspsaga")
+require("plugin-config/symbols_outline")
+require("plugin-config/mason")
+require("plugin-config/dap-config")
+require("plugin-config/dap-ui")
+require('plugin-config/nvimgdb')
+require('plugin-config/gitsigns')
+--require('plugin-config/dap-cortex-debug')
+--
+-- 启用主题
+vim.cmd[[colorscheme tokyonight]]
+-- vim.cmd[[colorscheme vscode]]
+
+-- 自定义状态栏(使用 lualine 插件后，该部分代码取消)
+-- function get_mode()
+--     local mode = vim.api.nvim_eval([[mode()]])
+--     if mode == "n" then
+--         return "Normal"
+--     elseif mode == "v" then
+--         return "Visual"
+--     elseif mode == "i" then
+--         return "Insert"
+--     else
+--         return ""
+--     end
+-- end
+-- 
+-- vim.o.statusline = "mode|%f%m|%l:%L%=" .. vim.g.encoding .. "|%y"
+-- 
+-- local status_line = vim.api.nvim_create_augroup("SET_STATUS_LINE", {clear = true})
+-- 
+-- -- 打开时获取当前模式，并初始化
+-- vim.api.nvim_create_autocmd({"ModeChanged"}, {
+--     pattern = "*",
+--     group = status_line,
+--     callback = function()
+--         local win_id = vim.api.nvim_eval([[win_getid()]]) --获取当前window id
+--         vim.wo[win_id].statusline = get_mode() .. "|%f%m|%l:%L%=" .. vim.g.encoding .. "|%y"
+--     end
+-- })
---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello, just a test!\n");
+    for (size_t i = 0; i < 10; i++) {
+        printf("i = %ld\n", i);
+    }
+    printf("bye!\n");
+    return 0;
+}
+
