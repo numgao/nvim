3f75035539061574d42444dad607c2393893af65
gao <paul.gao@gmail.com>
2024-04-04 14:07:28 +0800

    insert the ggleterm

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -37,7 +37,7 @@ return require('packer').startup(function(use)
         -- requires = {'nvim-tree/nvim-web-devicons'}
     }
     use {"folke/which-key.nvim"}
-   -- use {"akinsho/toggleterm.nvim", tag = 'v2.*'}
+    use {"akinsho/toggleterm.nvim", tag = 'v2.*'}
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         requires = { {'nvim-lua/plenary.nvim'} , 
