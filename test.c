f1c8c90a68011f1346df6aa6f254ee45e3e5b76d
gao <paul.gao@gmail.com>
2024-04-06 16:04:04 +0800

    change test.c

--- test.c
+++ test.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+int sub(void);
+
 int main() {
     printf("hello, just a test!\n");
     for (size_t i = 0; i < 10; i++) {
         printf("i = %ld\n", i);
     }
-    printf("bye!\n");
+    int c = sub();
+    printf("from sub c = %d\n", c);
     printf("bye again\n");
     printf("bye again!\n");
     return 0;
+}
+
+int sub(void){
+    printf("This is from the sub!\n");
+    int a = 1;
+    int b = 2;
+    return(a + b);
 }
 
